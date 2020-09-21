78532f8adb088f187a7363121661d01dc96b4f3e
qq876043667 <876043667@qq.com>
2020-09-21 13:43:37 +0800

    20200921

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class OrderHystrixController {
+
+    @Autowired
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping(value = "/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    };
+
+    @GetMapping(value = "/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_timeout(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_timeout(id);
+        return result;
+    };
+
+}
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -5,12 +5,16 @@ import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.lb.LoadBalance;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -25,6 +29,9 @@ public class OrderController {
     @Resource
     private LoadBalance loadBalance;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
 
@@ -36,6 +43,20 @@ public class OrderController {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" +id, CommonResult.class);
     }
 
+
+    @GetMapping("/consumer/payment/lb")
+    public String getPaymentLB() {
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if(instances == null || instances.size() <= 0) {
+            return null;
+        }
+
+        ServiceInstance serviceInstance = loadBalance.instance(instances);
+        URI uri = serviceInstance.getUri();
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
+
+    }
 
 
 }
